c3a6dfe72c07b06d9a2ac90027b42a26005fd57c
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-04-29 22:26:56 -0300

    Alterações na função

--- miniblog/src/hooks/useInsertDocument.js
+++ miniblog/src/hooks/useInsertDocument.js
@@ -60,9 +60,9 @@ export const useInsertDocument = (docCollection) =>{
     }
    };
 
-   useEffect(() => {
-    return() => setCancelled(true);
-   },[]);
+  //  useEffect(() => {
+  //   return() => setCancelled(true);
+  //  },[]);
 
    return {insertDocument,response};
 }
