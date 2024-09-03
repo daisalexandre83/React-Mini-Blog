29311ffff60f2fb607d4eb33237d2bfa7fd3bc8b
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-09-03 20:46:30 -0300

    Alterações no AuthContext.js

--- miniblog/src/context/AuthContext.js
+++ miniblog/src/context/AuthContext.js
@@ -4,6 +4,7 @@ const AuthContext = createContext()
 
 export function AuthProvider({children,value}) {
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    
 }
 
 export function useAuthValue() {
