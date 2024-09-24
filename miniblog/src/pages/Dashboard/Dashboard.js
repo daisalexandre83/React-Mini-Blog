5ad84478e77796f4cc150ac784750e078e7226e6
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-09-23 22:45:28 -0300

    Inserção e alteração na Dashboard

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -7,11 +7,17 @@ import {useAuthValue} from '../../context/AuthContext';
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const {user} = useAuthValue();
+  const uid = user.uid;
 
   const {documents:posts,loading} = useFetchDocuments("posts",null,uid);
 
+  const deleteDocument = (id) =>{
+    
+    if (loading) {
+      return<p>Carregando...</p>;
+    }
+  }
   return (
     <div>
         <h1>Dashboard</h1>
@@ -22,14 +28,35 @@ const Dashboard = () => {
           <Link to="/posts/create" className="btn">Criar primeiro post</Link>
          </div>
         ):(
-          <div>
-            <p>Tem posts!</p>
-          </div>
+          <>
+           <div>
+            <span>Título</span>
+            <span>Ações</span>
+           </div>
+           {posts && 
+             posts.map((post) => <div key={post.id}>
+             <p>{post.title}</p>
+             <div>
+              <Link to={`posts/${post.id}`} className="btn btn-outline">
+               Ver
+              </Link>
+              <Link to={`posts/edit/${post.id}`} className="btn btn-outline">
+               Editar
+              </Link>
+              <Link onClick={()=> deleteDocument(post.id)}
+               className="btn btn-outline btn-danger"
+              >
+               Excluir
+              </Link>
+             </div>
+           </div>
+          )}
+          </>
         )}
 
-        {posts && posts.map((post) => <h3>{post.title}</h3>)}
+        
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard
