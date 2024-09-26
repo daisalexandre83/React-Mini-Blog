a768d61ba15337b620af33ca798d2fad05c308ff
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2024-09-25 22:26:19 -0300

    Inserção no Dashboard.js

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
     }
   }
   return (
-    <div>
+    <div className={styles.dashboard}>
         <h1>Dashboard</h1>
         <p>Gerencie os seus posts</p>
         {posts && posts.length === 0 ?(
@@ -29,12 +29,12 @@ const Dashboard = () => {
          </div>
         ):(
           <>
-           <div>
+           <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
            </div>
            {posts && 
-             posts.map((post) => <div key={post.id}>
+             posts.map((post) => <div key={post.id} className={styles.post_row}>
              <p>{post.title}</p>
              <div>
               <Link to={`posts/${post.id}`} className="btn btn-outline">
